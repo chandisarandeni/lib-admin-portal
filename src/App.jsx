84cc7029fc1855242a3a035a1561b77a03d1e511
chandisarandeni <chandisarandeni2003@gmail.com>
2025-06-30 21:05:49 +0530

    feat: implement loginPage and set up routing ✅

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,11 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
+import LoginPage from "./pages/LoginPage";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <>
-      <div className="flex items-center justify-center min-h-screen bg-gray-100">
-        <div className="text-center bg-white p-10 rounded-xl shadow-md">
-          <h1 className="text-3xl font-semibold text-gray-800">
-            Tailwind CSS is working!
-          </h1>
-        </div>
-      </div>
-    </>
+    <div>
+      <LoginPage />
+    </div>
   );
 }
 
